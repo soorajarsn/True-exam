efb63838b1244293feae4ef477ef25fd5a846254
Sooraj Shukla <soorajshukla974@gmail.com>
2020-09-30 23:04:21 +0530

    add react and parcel

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../styles/theme.scss";
+function App() {
+    return (
+        <React.Fragment>
+            <h1>Hello TrueExam Coorporation</h1>
+        </React.Fragment>
+    )
+}
+
+export default App;
