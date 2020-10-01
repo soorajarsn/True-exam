c2c40027479297a4906e0f6792ba099f623afc9c
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-01 15:45:34 +0530

    add gradient on hero section

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import Navbar from './Navbar';
-import "../styles/theme.scss";
+import React from "react";
+import Navbar from "./Navbar";
 import indexHero from "./images/Index.webp";
+import "../styles/index.scss";
 function Index() {
-    return (
-        <React.Fragment>
-            <Navbar />
-            <div className="img-container">
-                <img src={indexHero} alt="" />
-            </div>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <div className="index-main">
+        <Navbar />
+        <div className="hero-img-container">
+          <img src={indexHero} alt="" />
+          <div className="gradient"></div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Index;
