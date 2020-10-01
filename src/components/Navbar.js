d753a39337e47af560ef5073c43f44db1c2a75da
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-01 14:57:23 +0530

    navbar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,60 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  console.log(Link);
+  function toggleNav(event) {
+    const button = event.currentTarget;
+    const id = button.getAttribute("data-target");
+    const hidden = document.querySelector(`#${id}`).getAttribute("aria-hidden");
+    hidden === "true" ? document.querySelector(`#${id}`).setAttribute("aria-hidden", false) : document.querySelector(`#${id}`).setAttribute("aria-hidden", true);
+    button.querySelector("i").classList.toggle("fa-times");
+  }
   return (
     <React.Fragment>
-      <div className="container nav-container">
-        <nav className="limit-width">
+      <div className="container nav-container flex">
+        <nav className="limit-width full-width">
           <ul className="flex justify-space-between">
             <li className="logo-container">
-              <Link to="/"><h3 className="smaller">Company Logo</h3></Link>
+              <Link to="/">
+                <h3 className="smaller">Company Logo</h3>
+              </Link>
             </li>
-            <li className="flex justify-space-between button-and-nav-links-container">
-              <div className="nav-items-container">
-                <ul className="nav-items flex larger-font full-width justify-space-between">
+            <li id="nav-hidden" className="button-and-nav-links-container" aria-hidden="true">
+              <div className="wrapper flex justify-space-between ">
+                <div className="nav-items-container">
+                  <ul className="nav-items flex larger-font full-width justify-space-between">
                     <li className="sm-margin-left sm-margin-right">
-                        <Link to="/">Lorem</Link>
+                      <Link to="/">Courses</Link>
                     </li>
                     <li className="sm-margin-left sm-margin-right">
-                        <Link to="/">Ipsum</Link>
+                      <Link to="/">Instructors</Link>
                     </li>
                     <li className="sm-margin-left sm-margin-right">
-                        <Link to="/">FAQs</Link>
+                      <Link to="/">FAQs</Link>
                     </li>
                     <li className="sm-margin-left sm-margin-right">
-                        <Link to="/">About</Link>
+                      <Link to="/">About</Link>
                     </li>
-                </ul>
-              </div>
-              <div className="flex">
+                  </ul>
+                </div>
+                <div className="flex button-group">
                   <div className="button-container sm-margin-right">
-                      <button type="button" className="button-primary">Login</button>
+                    <button type="button" className="button-primary">
+                      Login
+                    </button>
                   </div>
                   <div className="button-container sm-margin-left">
-                      <button type="button" className="button-secondary">Register</button>
+                    <button type="button" className="button-secondary">
+                      Register
+                    </button>
                   </div>
+                </div>
               </div>
+            </li>
+            <li className="lg-hidden">
+              <button type="button" data-target="nav-hidden" className="flex" onClick={toggleNav}>
+                <i className="fas fa-bars" />
+              </button>
             </li>
           </ul>
         </nav>
