8313b0de16b3d3d4a59fcafdb32ac7d3a309aff3
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-01 20:24:58 +0530

    add Pricing component to homepage

--- src/components/Index.js
+++ src/components/Index.js
@@ -2,15 +2,17 @@ import React from "react";
 import Navbar from "./Navbar";
 import indexHero from "./images/Index.webp";
 import "../styles/index.scss";
+import Pricing from "./Pricing";
 function Index() {
   return (
     <React.Fragment>
-      <div className="index-main">
+      <div className="index-main full-width">
         <Navbar />
         <div className="hero-img-container">
           <img src={indexHero} alt="" />
           <div className="gradient"></div>
         </div>
+        <Pricing />
       </div>
     </React.Fragment>
   );
