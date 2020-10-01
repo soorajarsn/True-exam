b8c795fdedbb445be192f05e22f26c541b7e0f9a
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-01 10:50:04 +0530

    add Index.js

---
+++ src/components/Index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+import "../styles/theme.scss";
+function Index() {
+    return (
+        <React.Fragment>
+            <Navbar />
+            <h1>Hello TrueExam Coorporation</h1>
+        </React.Fragment>
+    )
+}
+
+export default Index;
