ba61c7066e8723cabf1a7725c49dfe769c2d4e67
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-01 14:58:06 +0530

    add hero image to homepage

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
 import "../styles/theme.scss";
+import indexHero from "./images/Index.jpg";
 function Index() {
     return (
         <React.Fragment>
             <Navbar />
-            <h1>Hello TrueExam Coorporation</h1>
+            <img src={indexHero} alt="" />
         </React.Fragment>
     )
 }
