4c215e213756ebf91e63a8995c20d1ebc8408832
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-01 23:21:45 +0530

    update Rating.js

--- src/components/Ratings.js
+++ src/components/Ratings.js
@@ -3,72 +3,72 @@ import React from "react";
 function Ratings() {
   return (
     <div className="rating-section-container-main full-width flex">
-      <div class="rating-section full-width limit-width">
+      <div className="rating-section full-width limit-width">
         <h2 className="lg-margin">Trusted by 10 Lakhs+ users every month</h2>
         <p className="md-margin">TrueExam provides the best guidance to students. We began in 2009 and today we help more than 10 Lakhs learners every month.</p>
-        <div class="lg-margin rating-container">
-          <div class="rating flex flex-column color-secondary">
+        <div className="lg-margin rating-container">
+          <div className="rating flex flex-column color-secondary">
             <h3 className="sm-margin">4.5</h3>
             <ul className="flex">
               <li>
-                <i aria-hidden="true" class="fa fa-star"></i>
+                <i aria-hidden="true" className="fa fa-star"></i>
               </li>
               <li>
-                <i aria-hidden="true" class="fa fa-star"></i>
+                <i aria-hidden="true" className="fa fa-star"></i>
               </li>
               <li>
-                <i aria-hidden="true" class="fa fa-star"></i>
+                <i aria-hidden="true" className="fa fa-star"></i>
               </li>
               <li>
-                <i aria-hidden="true" class="fa fa-star"></i>
+                <i aria-hidden="true" className="fa fa-star"></i>
               </li>
               <li>
-                <i class="fas fa-star-half-alt"></i>
+                <i className="fas fa-star-half-alt"></i>
               </li>
             </ul>
             <p>1000+ Reviews</p>
           </div>
-          <ul class="rating-lines">
-            <li class="line-container flex color-secondary lg-margin">
+          <ul className="rating-lines">
+            <li className="line-container flex color-secondary lg-margin">
               <span className="bold">5</span>
-              <div class="line"></div>
+              <div className="line"></div>
             </li>
-            <li class="line-container flex color-secondary lg-margin">
+            <li className="line-container flex color-secondary lg-margin">
               <span className="bold">4</span>
-              <div class="line line-2"></div>
+              <div className="line line-2"></div>
             </li>
-            <li class="line-container flex color-secondary lg-margin">
+            <li className="line-container flex color-secondary lg-margin">
               <span className="bold">3</span>
-              <div class="line line-3"></div>
+              <div className="line line-3"></div>
             </li>
-            <li class="line-container flex color-secondary lg-margin">
+            <li className="line-container flex color-secondary lg-margin">
               <span className="bold">2</span>
-              <div class="line line-4"></div>
+              <div className="line line-4"></div>
             </li>
-            <li class="line-container flex color-secondary lg-margin">
+            <li className="line-container flex color-secondary lg-margin">
               <span className="bold">1</span>
-              <div class="line line-5"></div>
+              <div className="line line-5"></div>
             </li>
           </ul>
         </div>
-        <div class="overview-container lg-margin">
-          <div class="overview color-secondary flex flex-column bold">
-            <i class="fas fa-users"></i>
+        <div className="overview-container lg-margin">
+          <div className="overview color-secondary flex flex-column bold">
+            <i className="fas fa-users"></i>
             <h4 className="sm-margin">2 Crore+</h4>
             <span>Monthly Customers</span>
           </div>
-          <div class="overview color-secondary flex flex-column bold">
-            <i class="fas fa-user-md"></i>
+          <div className="overview color-secondary flex flex-column bold">
+            <i className="fas fa-user-md"></i>
             <h4 className="sm-margin">100,000+</h4>
             <span>Verified Doctors</span>
           </div>
-          <div class="overview color-secondary flex flex-column bold">
-            <i class="fas fa-pills"></i>
+          <div className="overview color-secondary flex flex-column bold">
+            <i className="fas fa-pills"></i>
             <h4 className="sm-margin">10 Lakh+</h4>
             <span>Medicine </span>
           </div>
-          <div class="overview color-secondary flex flex-column bold">
-            <i class="far fa-calendar"></i>
+          <div className="overview color-secondary flex flex-column bold">
+            <i className="far fa-calendar"></i>
             <h4 className="sm-margin">5 Crore+</h4>
             <span>Doctor Appointments</span>
           </div>
