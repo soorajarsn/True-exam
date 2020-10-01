dfcc7164d4be54a15b3e747f7267a3cd52114b2f
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-01 11:34:30 +0530

    modify navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,29 +11,31 @@ function Navbar() {
             <li className="logo-container">
               <Link to="/"><h3 className="smaller">Company Logo</h3></Link>
             </li>
-            <li className="sm-hidden lg-visible nav-items-container">
-              <ul className="nav-items flex larger-font full-width justify-space-between">
-                  <li className="sm-margin-left sm-margin-right">
-                      <Link to="/">Lorem</Link>
-                  </li>
-                  <li className="sm-margin-left sm-margin-right">
-                      <Link to="/">Ipsum</Link>
-                  </li>
-                  <li className="sm-margin-left sm-margin-right">
-                      <Link to="/">FAQs</Link>
-                  </li>
-                  <li className="sm-margin-left sm-margin-right">
-                      <Link to="/">About</Link>
-                  </li>
-              </ul>
-            </li>
-            <li className="flex">
-                <div className="button-container sm-margin-right">
-                    <button type="button" className="button-primary">Login</button>
-                </div>
-                <div className="button-container sm-margin-left">
-                    <button type="button">Register</button>
-                </div>
+            <li className="flex justify-space-between button-and-nav-links-container">
+              <div className="nav-items-container">
+                <ul className="nav-items flex larger-font full-width justify-space-between">
+                    <li className="sm-margin-left sm-margin-right">
+                        <Link to="/">Lorem</Link>
+                    </li>
+                    <li className="sm-margin-left sm-margin-right">
+                        <Link to="/">Ipsum</Link>
+                    </li>
+                    <li className="sm-margin-left sm-margin-right">
+                        <Link to="/">FAQs</Link>
+                    </li>
+                    <li className="sm-margin-left sm-margin-right">
+                        <Link to="/">About</Link>
+                    </li>
+                </ul>
+              </div>
+              <div className="flex">
+                  <div className="button-container sm-margin-right">
+                      <button type="button" className="button-primary">Login</button>
+                  </div>
+                  <div className="button-container sm-margin-left">
+                      <button type="button">Register</button>
+                  </div>
+              </div>
             </li>
           </ul>
         </nav>
