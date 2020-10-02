87e02640fbe8824d89813912804d3b15ad1f54ae
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-02 19:32:24 +0530

    add assigments route

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React from "react";
 import "../styles/theme.scss";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Index from './Index';
-import { LoginWithEmail } from './Login';
-import Signup from './Signup';
-import Store from '../state/Store';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Index from "./Index";
+import { LoginWithEmail } from "./Login";
+import Signup from "./Signup";
+import Store from "../state/Store";
+import Assignments from "./Assignments";
 function App() {
-    return (
-        <React.Fragment>
-            <Router>
-                <Route exact={true} path="/" render={props => <Index {...props} />}/>
-                <Route exact={true} path="/login/:method?" render={props => <LoginWithEmail {...props} />} />
-                <Route exact={true} path="/signup" render={props => <Signup {...props} />} />
-            </Router>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <Router>
+        <Route exact={true} path="/" render={props => <Index {...props} />} />
+        <Route exact={true} path="/login/:method?" render={props => <LoginWithEmail {...props} />} />
+        <Route exact={true} path="/signup" render={props => <Signup {...props} />} />
+        <Route exact={true} path="/assignments" render={props => <Assignments {...props} />} />
+      </Router>
+    </React.Fragment>
+  );
 }
-function AppWithStore(){
-    return <Store>
+function AppWithStore() {
+  return (
+    <Store>
       <App />
     </Store>
-  }
-  
+  );
+}
+
 export default AppWithStore;
