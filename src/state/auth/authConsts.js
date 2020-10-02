068e75b9413372af066345070579710e0a691906
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-02 16:21:27 +0530

    define auth consts

---
+++ src/state/auth/authConsts.js
@@ -0,0 +1,12 @@
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_REQUEST = "LOGIN_REQUEST";
+export const LOGIN_ERROR = 'LOGIN_ERROR';
+
+export const SIGNUP_REQUEST = 'SIGNUP_REQUEST';
+export const SIGNUP_ERROR = 'SIGNUP_ERROR';
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+
+export const LOAD_USER_SUCCESS = "LOAD_USER_SUCCESS";
+export const LOAD_USER_ERROR = 'LOAD_USER_ERROR';
+
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
