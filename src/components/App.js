ea3cc91c7938cba4ae4414dbcdba21567eafeb24
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-02 10:26:01 +0530

    add LoginWithEmail route

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import "../styles/theme.scss";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Index from './Index';
+import { LoginWithEmail } from './Login';
 function App() {
     return (
         <React.Fragment>
             <Router>
                 <Route exact={true} path="/" render={props => <Index {...props} />}/>
+                <Route exact={true} path="/login" render={props => <LoginWithEmail {...props} />} />
             </Router>
         </React.Fragment>
     )
