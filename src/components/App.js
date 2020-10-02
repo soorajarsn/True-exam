312b06342dd8aa738cccf764c93bea233acb5acf
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-02 14:29:46 +0530

    add signup route

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import "../styles/theme.scss";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Index from './Index';
 import { LoginWithEmail } from './Login';
+import Signup from './Signup';
 function App() {
     return (
         <React.Fragment>
             <Router>
                 <Route exact={true} path="/" render={props => <Index {...props} />}/>
                 <Route exact={true} path="/login/:method?" render={props => <LoginWithEmail {...props} />} />
+                <Route exact={true} path="/signup" render={props => <Signup {...props} />} />
             </Router>
         </React.Fragment>
     )
