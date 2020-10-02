7fd9f93f56676408eb9ee1a3e67ffcd6d9b73821
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-03 00:25:56 +0530

    remove hero-section from only-instructor view to both

--- src/components/AssignmentViewInstructor.js
+++ src/components/AssignmentViewInstructor.js
@@ -1,23 +1,8 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import img1 from "./images/img1.jpg";
-import img2 from "./images/img2.jpg";
 function AssignmentViewInstructor() {
   return (
     <div className="assignment-container-instructor-main full-width flex flex-column">
-      <div className="hero full-width">
-        <div className="hero-img-container">
-          <img className=" sm-visible md-visible" src={img1} alt="" />
-          {/* <img className="hidden sm-visible" src={img2} alt="" /> */}
-        </div>
-        <div className="overlay"></div>
-        <div className="content-container container full-width flex">
-          <div className="content flex flex-column limit-width full-width">
-            <h1>Assignments</h1>
-            <span>Home/Assginments</span>
-          </div>
-        </div>
-      </div>
       <div className="assignment-create full-width flex">
         <div className="create flex flex-column container full-width limit-width">
           <h2 className="">Create Assignment</h2>
