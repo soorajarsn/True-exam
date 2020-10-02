fab3b114cf41b02ea5788320657b04af9699eca9
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-02 16:28:16 +0530

    define actions

---
+++ src/state/auth/authActions.js
@@ -0,0 +1,29 @@
+import { LOGIN_SUCCESS, 
+         LOGIN_ERROR, 
+         LOGIN_REQUEST, 
+         SIGNUP_REQUEST, 
+         SIGNUP_SUCCESS,
+         SIGNUP_ERROR,
+         LOGOUT_SUCCESS} from './authConsts';
+
+
+
+export const loginRequest = () => ({type:LOGIN_REQUEST});
+export const loginError = (err) => ({type:LOGIN_ERROR,payload:err});
+export const loginSuccess = (userData) => ({type:LOGIN_SUCCESS,payload:userData});
+
+export const signupRequest = () => ({type:SIGNUP_REQUEST});
+export const signupError = (err) => ({type:SIGNUP_ERROR,payload:err});
+export const signupSuccess = (userData) => ({type:SIGNUP_SUCCESS,payload:userData});
+
+export const logOut = () => ({type:LOGOUT_SUCCESS});
+
+export const loginUser = (dispatch,body) => {
+        dispatch(loginRequest());
+        dispatch(loginSuccess({token:'sfk23492jsdlfwr',isInstructor:body.instructor}));
+}
+
+export const signupUser = (dispatch, body) => {
+        dispatch(signupRequest());
+        dispatch(signupSuccess({token:'sfk23492jsdlfwr',isInstructor:body.instructor}))
+}
