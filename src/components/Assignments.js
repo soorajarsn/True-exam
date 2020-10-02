f4c0303b45051435a35466a09ceec99d51bceb8d
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-03 00:09:50 +0530

    add instructor view of assignment page

--- src/components/Assignments.js
+++ src/components/Assignments.js
@@ -3,7 +3,8 @@ import { AuthContext } from "../state/Store";
 import { Redirect } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-
+import AssignmentViewInstructor from "./AssignmentViewInstructor";
+import "../styles/assignment.scss";
 function Assignments(props) {
   const auth = useContext(AuthContext);
   return (
@@ -11,8 +12,9 @@ function Assignments(props) {
       {!auth.state.userLoggedIn ? (
         <Redirect to={{ pathname: "/login", state: { from: props.location } }} />
       ) : (
-        <div className="assigment-container-main">
+        <div className="assignment-container-main">
           <Navbar />
+          <AssignmentViewInstructor />
           <Footer />
         </div>
       )}
