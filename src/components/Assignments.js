3e3c9b55faae3608890edaee6f926e559c823b94
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-02 19:31:03 +0530

    add Nav and Footer in Assignment.js

--- src/components/Assignments.js
+++ src/components/Assignments.js
@@ -1,10 +1,23 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../state/Store";
 import { Redirect } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function Assignments(props) {
   const auth = useContext(AuthContext);
-  return <React.Fragment>{auth.state.userLoggedIn ? <Redirect to={{ pathname: "/login", state: { from: props.location } }} /> : <div></div>}</React.Fragment>;
+  return (
+    <React.Fragment>
+      {auth.state.userLoggedIn ? (
+        <Redirect to={{ pathname: "/login", state: { from: props.location } }} />
+      ) : (
+        <div className="assigment-container-main">
+          <Navbar />
+          <Footer />
+        </div>
+      )}
+    </React.Fragment>
+  );
 }
 
 export default Assignments;
