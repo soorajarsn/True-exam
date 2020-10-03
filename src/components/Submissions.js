83206dd9f0bd5f939476e93453e9c846ce282753
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-03 23:35:33 +0530

    add hero section and instructor verification : submission page

--- src/components/Submissions.js
+++ src/components/Submissions.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AuthContext } from "../state/Store";
+import { Redirect } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import img1 from "./images/img1.jpg";
 
 function Submissions(props) {
-    
-    return (
-        <div>
-            
-        </div>
-    )
+  const auth = useContext(AuthContext);
+  return (
+    <React.Fragment>
+      {!auth.state.userLoggedIn ? (
+        <Redirect to={{ pathname: "/login", state: { from: props.location } }} />
+      ) : (
+        <React.Fragment>
+          {!auth.state.isInstructor ? (
+            <div>Access Denied: Instructor Only Access Permissions</div>
+          ) : (
+            <div className="assignment-container-main">
+              <Navbar />
+              <div className="hero full-width">
+                <div className="hero-img-container">
+                  <img className=" sm-visible md-visible" src={img1} alt="" />
+                </div>
+                <div className="overlay"></div>
+                <div className="content-container container full-width flex">
+                  <div className="content flex flex-column limit-width full-width">
+                    <h1>Submissions</h1>
+                    <span>Home/Assginments/Submissions</span>
+                  </div>
+                </div>
+              </div>
+              <Footer />
+            </div>
+          )}
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
 }
 
-export default Submissions
-props
+export default Submissions;
