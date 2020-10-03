2801c2492557ae3cf78062db89b24761a9903d36
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-03 15:27:05 +0530

    createAssignment complete

---
+++ src/components/CreateAssignmentForm.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import "../styles/createAssignment.scss";
+const useInput = input => {
+//   const [value, setValue] = useState("");
+  const inputElement = (
+    <div className={"input-container lg-margin" + (input.containerClassName ? input.containerClassName : "")}>
+      <input
+        type={input.type}
+        name={input.name}
+        placeholder={input.placeholder}
+        className="lg-padding sm-margin"
+        autoComplete="off"
+        // value={value}/
+        // onChange={event => setValue(event.target.value)}
+      />
+    </div>
+  );
+  return inputElement;
+};
+const createInputs = n => {
+  let inputArray = [];
+  for (var i = 0; i < n; i++) inputArray.push(useInput({ type: "text", name: "instruction" + (i + 1), placeholder: "Instruction" + (i + 1) }));
+  return inputArray;
+};
+function CreateAssignmentForm() {
+  const [inputCount, setInputCount] = useState(5);
+  let inputs = createInputs(inputCount);
+  const increaseInputCount = () => {
+      setInputCount(prev => prev+1);
+  }
+  useEffect(()=>{
+    const clipHeight = () => {
+        const body = document.querySelector('body');
+        body.style.height = "200vh";
+        body.style.overflow = "hidden";
+    }
+    const unClipHeight = ()=> {
+        const body = document.querySelector('body');
+        body.style.height = "unset";
+        body.style.overflow = "unset";
+    }
+    clipHeight();
+  })
+  return (
+    <div className="assignment-form-container-main flex">
+      <div className="container form-container flex">
+        <form className="create-assignment-form">
+          {inputs.map((Input, index) => (
+            <div key={index}>{Input}</div>
+          ))}
+          <div className="add-more-container flex full-width justify-start">
+            <button className="add-more bold md-padding" type="button" onClick={increaseInputCount}>
+              Add more Instructions
+            </button>
+          </div>
+          <div className="upload-file-container flex full-width justify-start lg-margin-top">
+            <button className="file-upload-button bold md-padding" type="button">
+              Upload Image File
+            </button>
+            <input type="file" accept="image/*" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default CreateAssignmentForm;
