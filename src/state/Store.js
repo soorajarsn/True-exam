04b84637032653f877dbb97ca6a85941d530178e
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-04 09:30:02 +0530

    create context of assignments and provide to app

--- src/state/Store.js
+++ src/state/Store.js
@@ -1,12 +1,15 @@
-import React,{useReducer} from "react";
-import {initialAuthState, authReducer} from './auth/authReducer';
-
+import React, { useReducer } from "react";
+import { initialAuthState, authReducer } from "./auth/authReducer";
+import { assignmentReducer, initialAssignments } from "./assignments/assignmentReducer";
 export const AuthContext = React.createContext();
-
-function Store(props){
-    const [auth,dispatch] = useReducer(authReducer,initialAuthState);
-    return <AuthContext.Provider value={{state:auth,dispatch}}>
-        {props.children}
+export const AssignmentContext = React.createContext();
+function Store(props) {
+  const [auth, dispatch] = useReducer(authReducer, initialAuthState);
+  const [assignments, dispatch] = useReducer(assignmentReducer, initialAssignments);
+  return (
+    <AuthContext.Provider value={{ state: auth, dispatch }}>
+      <AssignmentContext.Provider value={{ state: assignments, dispatch }}>{props.children}</AssignmentContext.Provider>
     </AuthContext.Provider>
+  );
 }
 export default Store;
