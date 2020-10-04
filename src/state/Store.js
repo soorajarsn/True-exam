d4041e68cb74adf5a1ff86962be531c8f6f23be6
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-04 09:31:46 +0530

    remove bug: duplicate variable declaration in Store.js

--- src/state/Store.js
+++ src/state/Store.js
@@ -5,10 +5,10 @@ export const AuthContext = React.createContext();
 export const AssignmentContext = React.createContext();
 function Store(props) {
   const [auth, dispatch] = useReducer(authReducer, initialAuthState);
-  const [assignments, dispatch] = useReducer(assignmentReducer, initialAssignments);
+  const [assignments, dispatchAssignment] = useReducer(assignmentReducer, initialAssignments);
   return (
     <AuthContext.Provider value={{ state: auth, dispatch }}>
-      <AssignmentContext.Provider value={{ state: assignments, dispatch }}>{props.children}</AssignmentContext.Provider>
+      <AssignmentContext.Provider value={{ state: assignments, dispatch:dispatchAssignment }}>{props.children}</AssignmentContext.Provider>
     </AuthContext.Provider>
   );
 }
