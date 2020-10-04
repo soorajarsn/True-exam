b1385ea7d66a21075de4c6372408c6de81e17fad
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-04 11:35:11 +0530

    add update score functionality

--- src/components/Submissions.js
+++ src/components/Submissions.js
@@ -14,27 +14,36 @@ function Options() {
 }
 function SubmissionCard(props) {
   const [selectValue, setSelectValue] = useState(1);
-  const {templateAssignments,dispatchAssignment} = props;
+  const { templateAssignments, dispatchAssignment, assignment, submission } = props;
+  const processAssignment = (assignment, score) => {
+    if (assignment.assignmentId == props.assignmentId) {
+      let updatedSub = assignment.submissions.map(sub => (sub.userId == submission.userId && { ...sub, score }) || sub);
+      return { ...assignment, submissions: updatedSub };
+    } else return assignment;
+  };
   const updateScore = event => {
     const newScore = event.target.value;
     setSelectValue(newScore);
     let asmt = templateAssignments.filter(assignment => assignment.assignmentId == props.assignmentId);
     if (asmt.length == 1) {
-      let newTemplateAssignments = templateAssignments.map(assignment => (assignment.assignmentId == props.assignmentId && { ...assignment, score: newScore }) || assignment);
-      updateAssignment(dispatchAssignment,newTemplateAssignments);
+      let newTemplateAssignments = templateAssignments.map(assignment => processAssignment(assignment, newScore));
+      console.log("New Assignment ", newTemplateAssignments);
+      updateAssignment(dispatchAssignment, { templateAssignments: newTemplateAssignments });
+      props.setLoadAssignments(true);
     } else {
       asmt = getLocalAssignments().map(assignment => (assignment.assignmentId == props.assignmentId && { ...assignment, score: newScore }) || assignment);
       localStorage.setItem("assignments", JSON.stringify(asmt));
+      console.log("SEt");
+      props.setLoadAssignments(true);
     }
-    props.setLoadAssignments(true);
   };
   return (
-    <div id={props.assignmentId} className="assignment lg-margin assignment-uploaded">
+    <div id={assignment.assignmentId} className="assignment lg-margin assignment-uploaded">
       <div className="content">
-        <h2 className="smaller">{props.title}</h2>
+        <h2 className="smaller">{assignment.title}</h2>
         <h3 className="sm-smaller md-smaller lg-smaller lg-margin-top md-padding-top">Instructions</h3>
         <ul className="lg-margin">
-          {props.instructions.map((instruction, index) => (
+          {assignment.instructions.map((instruction, index) => (
             <li key={index} className="md-margin sm-margin-right">
               {instruction}
             </li>
@@ -44,12 +53,12 @@ function SubmissionCard(props) {
           <ul className="flex jusitify-start">
             <li className="sm-margin-right">
               <p className="bold">
-                UserId:<span className="sm-margin-left">{props.userId}</span>
+                UserId:<span className="sm-margin-left">{submission.userId}</span>
               </p>
             </li>
             <li className="sm-margin-left">
               <p className="bold">
-                UserName:<span className="sm-margin-left">{props.userName}</span>
+                UserName:<span className="sm-margin-left">{submission.userName}</span>
               </p>
             </li>
           </ul>
@@ -57,7 +66,7 @@ function SubmissionCard(props) {
         <div className="flex justify-start score-container">
           <div className="score lg-margin-top flex justify-start lg-margin-right">
             <h3 className="sm-smaller md-smaller lg-smaller no-margin">Current Score:</h3>
-            <span className="md-margin-left sm-padding sm-padding-left sm-padding-right md-padding">{props.score}</span>
+            <span className="md-margin-left sm-padding sm-padding-left sm-padding-right md-padding">{submission.score}</span>
           </div>
           <div className="lg-margin-left score lg-margin-top flex justify-start">
             <h3 className="sm-smaller md-smaller lg-smaller no-margin">Score this Assignment:</h3>
@@ -69,8 +78,8 @@ function SubmissionCard(props) {
       </div>
       <div className="uploaded-img-container background-white flex">
         <div className="img-container flex">
-          <img src={props.image} alt="assignment-img" />
-          <a className="download-edited-img flex" href={props.image} download>
+          <img src={submission.image} alt="assignment-img" />
+          <a className="download-edited-img flex" href={submission.image} download>
             <i className="fas fa-download"></i>
           </a>
         </div>
@@ -81,16 +90,15 @@ function SubmissionCard(props) {
 function Submissions(props) {
   const auth = useContext(AuthContext);
   const { assignmentId } = props.match.params;
-  const [submissions, setSubmissions] = useState([]);
-  const [assignment, setAssignment] = useState({});
+  const [assignment, setAssignment] = useState({ submissions: [] });
   const [loadAssignments, setLoadAssignments] = useState(true);
-  const {assignments,dispatchAssignment} = useContext(AssignmentContext);
+  const { assignments, dispatchAssignment } = useContext(AssignmentContext);
   useEffect(() => {
     const intialize = asmt => {
       setAssignment(asmt);
-      setSubmissions(asmt.submissions || []);
     };
     if (loadAssignments) {
+      console.log(assignments);
       let asmt = assignments.templateAssignments.filter(assignment => assignment.assignmentId === assignmentId);
       if (asmt.length === 1) intialize(asmt[0]);
       else {
@@ -126,8 +134,11 @@ function Submissions(props) {
               <div className="assignment-container-student-main full-width flex flex-column">
                 <div className="assignments full-width limit-width flex">
                   <div className="container">
-                    {submissions.map(submission => (
-                      <SubmissionCard key={submission.userId} {...{dispatchAssignment,templateAssignments:assignments.templateAssignments,setLoadAssignments}} {...assignment} {...submission} />
+                    {assignment.submissions.map(submission => (
+                      <SubmissionCard
+                        key={submission.userId}
+                        {...{ dispatchAssignment, templateAssignments: assignments.templateAssignments, setLoadAssignments, assignment, submission, assignmentId }}
+                      />
                     ))}
                   </div>
                 </div>
